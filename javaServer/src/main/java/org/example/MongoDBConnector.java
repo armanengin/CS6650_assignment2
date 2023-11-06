3d2d0b2884b50fde6be0ab9e25479e64f106d03c
Arman Engin Sucu <63503839+armanengin@users.noreply.github.com>
2023-11-05 23:41:08 -0800

    Update MongoDBConnector.java

--- javaServer/src/main/java/org/example/MongoDBConnector.java
+++ javaServer/src/main/java/org/example/MongoDBConnector.java
@@ -10,8 +10,8 @@ public class MongoDBConnector {
     private static MongoDatabase database;
 
     static {
-        mongoClient = new MongoClient("ip-172-31-17-155.us-west-2.compute.internal", 27017); // Connect to your MongoDB server
-        database = mongoClient.getDatabase("cs6650DB"); // Use your database
+        mongoClient = new MongoClient("ip-172-31-17-155.us-west-2.compute.internal", 27017); // Connect to MongoDB server
+        database = mongoClient.getDatabase("cs6650DB");
     }
 
     public static MongoCollection<Document> getAlbumCollection() {
